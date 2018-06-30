f14d1f3dd79437b683ad2d4fa77c9ed19f81ea57
drenski666 <drenski666@hotmail.com>
2018-06-30 16:53:11 +0300

    Add project files.

---
+++ AmazoNET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AmazoNET.Startup))]
+namespace AmazoNET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
