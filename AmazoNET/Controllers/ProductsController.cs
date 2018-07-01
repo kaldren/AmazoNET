5ae605daeea6febcbcfc365127c39f89ac0bdbe9
drenski666 <drenski666@hotmail.com>
2018-07-01 09:16:09 +0300

    Added Product Form

---
+++ AmazoNET/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using AmazoNET.Models;
+using AmazoNET.ViewModels;
+
+namespace AmazoNET.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Products
+        public ActionResult Create()
+        {
+            var viewModel = new ProductFormViewModel
+            {
+                Categories = _context.Categories.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ AmazoNET/ViewModels/ProductFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using AmazoNET.Models;
+
+namespace AmazoNET.ViewModels
+{
+    public class ProductFormViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
